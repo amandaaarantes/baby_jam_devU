2a5f5d7c5386f66f2e904dba48039e2187dda548
Amanda Arantes <amandaaalmeida0609@gmail.com>
2025-07-15 13:43:26 -0300

    [ATT] finalizado o ataque de curta distancia do inimigo

--- Assets/AA_Scripts/ataquePertoPlayer.cs
+++ Assets/AA_Scripts/ataquePertoPlayer.cs
@@ -7,11 +7,17 @@ public class ataquePertoPlayer : MonoBehaviour
 {
     private Animator animatorPlayer;
     private Rigidbody2D rbPlayer;
+    public LayerMask Enemy;
+
+    [Header("Ataque")]
+    public int dano;
+
+    [Header("Zona de ataque")]
+    public Transform zonaAtaque;
+    public float raioAtaque;
 
     [Header("Intervalo entre os ataques")]
     public float cooldown;
-    private float proximoAtaque;
-
     private bool estaAtacando = false;
 
     void Start()
@@ -23,20 +29,42 @@ public class ataquePertoPlayer : MonoBehaviour
 
     void Update()
     {
-        
+
         if (Input.GetButtonDown("Fire1") && !estaAtacando && rbPlayer.linearVelocity.y == 0 && rbPlayer.linearVelocity.x == 0)
         {
             animatorPlayer.SetTrigger("Ataque");
+            Atacar();
             StartCoroutine(cooldownAtaque());
 
         }
-    }
 
+    }
 
     private IEnumerator cooldownAtaque()
     {
         estaAtacando = true;
         yield return new WaitForSecondsRealtime(cooldown);
         estaAtacando = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (zonaAtaque != null)
+        {
+            Gizmos.DrawWireSphere(zonaAtaque.position, raioAtaque);
+        }
+    }
+
+    private void Atacar()
+    {
+        Collider2D colliderInimigo = Physics2D.OverlapCircle(zonaAtaque.position, raioAtaque, Enemy);
+        if (colliderInimigo != null)
+        {
+            HealthSistem alvo = colliderInimigo.GetComponent<HealthSistem>();
+             if (alvo != null)
+             {
+                 alvo.TakeDamage(dano);
+             }
+        }
     }
 }
