ae196f54a775566194851ca4540bb52b6ff7d937
Amanda Arantes <amandaaalmeida0609@gmail.com>
2025-07-16 19:54:23 -0300

    [ADD] scripts: inicio da logica de coletaveis - totens

--- Assets/AA_Scripts/totens.cs
+++ Assets/AA_Scripts/totens.cs
@@ -5,6 +5,44 @@ using UnityEngine.UI;
 
 public class totens : MonoBehaviour
 {
+    public movPlayer movP;
+    public ataquePertoPlayer ataqueP;
+    public HealthSistem hsP;
+    public Animator animP;
 
-    // tres ifs, um para cada tipo de totem, 
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TotemForca"))
+        {
+
+            // mecanica de escolha
+            bool mexerDps = false;
+            if (mexerDps) { // escolhido + dano
+                ataqueP.dano += ataqueP.dano * 50 / 100;
+            }
+            else if (!mexerDps) { // escolhido + distancia de ataque
+                ataqueP.raioAtaque += ataqueP.raioAtaque * 30 / 100;
+            }
+
+            animP.SetTrigger("TotemForca");
+        }
+
+        else if (collision.CompareTag("TotemDefesa"))
+        {
+            // mecanica de escolha
+
+
+            hsP.vidaAtual = hsP.vidaMaxima;
+            hsP.coracoes.AtualizarCoracoes();
+
+            //logica para add +1 coracao 
+
+        }
+        else if (collision.CompareTag("TotemMobilidade"))
+        {
+            movP.quantPulos += 1;
+        }
+    }
+
 }
